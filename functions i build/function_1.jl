1bd1f8eda4cb793b8987d8f5946095b7d7e2831b
shubhendu ghosh <77840111+shubhendu-ghosh-DS@users.noreply.github.com>
2022-01-18 17:37:14 +0530

    Update function_1.jl

--- functions i build/function_1.jl
+++ functions i build/function_1.jl
@@ -1,1 +1,18 @@
-function 
+function SquareRoot(n)
+    p = 13      #this is the number of iteration
+    u = 1       
+    x = n - 2
+    while u < p
+        y = (1/2)*(x + n/x)
+        println(y)
+        if y == x         #stopping the function to iterate any further if it has converged
+            break
+        end
+        x = y
+        u = u + 1
+    end
+end
+
+
+#Calling the function
+SquareRoot(625)
