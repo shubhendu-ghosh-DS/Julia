2645552f1383a2718fba00760ff4ed4902edaa97
shubhendu ghosh <77840111+shubhendu-ghosh-DS@users.noreply.github.com>
2022-01-19 18:15:43 +0530

    Update bisection method.jl

--- functions i build/bisection method.jl
+++ functions i build/bisection method.jl
@@ -26,3 +26,41 @@ function bisection(f,a,b,iteration)
     end
     return p
 end
+
+
+
+
+#uploading some functions 
+function function_a(x)
+    val = x - sin(x) - 1/2
+    return val
+end
+
+function function_b(x)
+    val = x^6 - x^4 - x^3 - 1
+    return val
+end
+
+function function_c(x)
+    val = x^3 - 3*x - 5
+    return val
+end
+
+function function_d(x)
+    val = x*exp(x) - cos(x)
+    return val
+end
+
+function function_e(x)
+    val = tan(x) + x
+    return val
+end
+
+function function_f(x)
+    val = x + log(x) - 2
+    return val
+end
+
+#creating a vector of functions
+function_list = [function_a, function_b, function_c, function_d, function_e, function_f]
+
